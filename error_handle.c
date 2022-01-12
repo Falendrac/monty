c4bfdfe2b3c7ef4943877012ca4f6d371b23d936
Falendrac <leger_franck@hotmail.fr>
2022-01-12 14:35:39 +0000

    Fix format of error printed and the exit status of main

--- error_handle.c
+++ error_handle.c
@@ -23,16 +23,16 @@ void exit_procedure(int exitNum, int line_number, char **argv, char *opcode)
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	case 1:
-		fprintf(stderr, "Error: Can't open file <%s>\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	case 2:
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	case 3:
-		fprintf(stderr, "L<%d>: unknown instruction <%s>\n", line_number, opcode);
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 		exit(EXIT_FAILURE);
 	case 4:
-		fprintf(stderr, "L<%d>: usage: push integer\n", line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
