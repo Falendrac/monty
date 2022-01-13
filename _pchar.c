b6f33d7c248a26058d53293777ba4f4657ecba8f
jodt <joel.dumortier@gmail.com>
2022-01-13 11:10:36 +0000

    create and move a pointer instead of *stack

--- _pchar.c
+++ _pchar.c
@@ -7,6 +7,8 @@
 
 void _pchar(stack_t **stack, unsigned int line_number)
 {
+	stack_t *current;
+
 	if (stack == NULL || *stack == NULL)
 	{
 		_free_stack(stack);
@@ -16,7 +18,8 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 32 || (*stack)->n > 126)
+	current = *stack;
+	if (current->n < 32 || current->n > 126)
 	{
 		_free_stack(stack);
 		free_line_t();
@@ -25,5 +28,5 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n", (*stack)->n);
+	printf("%c\n", current->n);
 }
